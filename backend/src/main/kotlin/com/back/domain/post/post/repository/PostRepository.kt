4a141127db92fdf3ec02a7a24168e36d5cefd9c2
sat7312 <sat7312@gmail.com>
2026-05-06 10:32:30 +0900

    6 QueryDsl 적용 전 사전 작업

--- backend/src/main/kotlin/com/back/domain/post/post/repository/PostRepository.kt
+++ backend/src/main/kotlin/com/back/domain/post/post/repository/PostRepository.kt
@@ -3,4 +3,4 @@ package com.back.domain.post.post.repository
 import com.back.domain.post.post.entity.Post
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface PostRepository : JpaRepository<Post, Int>
+interface PostRepository : JpaRepository<Post, Int>, PostRepositoryCustom
